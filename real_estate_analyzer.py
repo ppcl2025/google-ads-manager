0f1d4b9b27ed1ddee232d84193731523e126b441
Jered Russell <jered.russell89@gmail.com>
2025-12-12 13:43:37 -0500

    Update margins and center-align metric content with larger fonts for square boxes

--- real_estate_analyzer.py
+++ real_estate_analyzer.py
@@ -824,11 +824,13 @@ def create_biweekly_report_pdf(report_content, account_name, campaign_name, date
                             name_escaped = escape(str(metric['name']))
                             value_escaped = escape(str(metric['value']))
                             
-                            cell_text = f"<font name='Helvetica' size='9' color='{COLOR_GRAY.hexval()}'>{name_escaped}</font><br/>"
-                            cell_text += f"<font name='Helvetica-Bold' size='18' color='{metric['color'].hexval()}'>{value_escaped}</font>"
+                            # Center align the content for better square appearance with larger fonts
+                            cell_text = f"<para alignment='center'><font name='Helvetica' size='10' color='{COLOR_GRAY.hexval()}'><b>{name_escaped}</b></font><br/><br/>"
+                            cell_text += f"<font name='Helvetica-Bold' size='20' color='{metric['color'].hexval()}'>{value_escaped}</font>"
                             if metric['description']:
                                 desc_escaped = escape(str(metric['description']))
-                                cell_text += f"<br/><font name='Helvetica' size='8' color='{COLOR_GRAY.hexval()}'>{desc_escaped}</font>"
+                                cell_text += f"<br/><br/><font name='Helvetica' size='9' color='{COLOR_GRAY.hexval()}'>{desc_escaped}</font>"
+                            cell_text += "</para>"
                             
                             cell_content = Paragraph(cell_text, body_style)
                             table_data.append([cell_content])
